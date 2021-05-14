d5e70ee8f6636a2e827fed5b5eb108764228ab5f
jrw289 <jrw289@cornell.edu>
2021-05-13 22:40:29 -0400

    Initial commit on main branch

---
+++ funcs.py
@@ -0,0 +1,13 @@
+def loop():
+    listy = []
+    for i in range(0,10):
+        listy.append(i)
+    return listy
+
+def list_comp():
+    listy = [i for i in range(0,10)]
+    return listy
+
+def ranges():
+    x = range(0,10)
+    return x
